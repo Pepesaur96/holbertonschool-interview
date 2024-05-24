a7b241cd524075f3c079edfecf95e416bd247b08
Pepesaur96 <jjvazquez96@gmail.com>
2024-05-24 19:10:29 -0400

    Task 1 / added heapt_t structure to header file 7:10pm 05/24/2024

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -11,16 +11,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t, heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
