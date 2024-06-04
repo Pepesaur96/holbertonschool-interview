dac5a50eb2b2693a31556f72d025e259d15efdec
Pepesaur96 <jjvazquez96@gmail.com>
2024-06-04 11:45:37 -0400

    Task 0 / Reworked

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,60 +1,116 @@
 #include "sandpiles.h"
-#include <stdio.h>
-
 /**
- * sandpiles_sum - computes the sum of two sandpiles
- * @grid1: first grid
- * @grid2: second grid
+ * print_grid2 - prints a grid
+ *
+ * @grid: grid to print
+ * Return: no return
+ *
+ */
+void print_grid2(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+/**
+ * add - sum two grids
+ * @grid1: 1st grid to sum
+ * @grid2: 2d grid to sum
+ * Return: no return
+ *
+ */
+void add(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+            grid1[i][j] += grid2[i][j];
+    }
+}
+/**
+ * tupple - excecutes one step tupple
+ * @grid: grid to analize
+ * @tmp: how much to add to grid sandpiles
+ * Return: no return
+ *
+ */
+void tupple(int grid[3][3], int tmp[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+            tmp[i][j] = 0;
+    }
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+            {
+                tmp[i][j] -= 4;
+                if (i + 1 < 3)
+                    tmp[i + 1][j] += 1;
+                if (j + 1 < 3)
+                    tmp[i][j + 1] += 1;
+                if (i - 1 >= 0)
+                    tmp[i - 1][j] += 1;
+                if (j - 1 >= 0)
+                    tmp[i][j - 1] += 1;
+            }
+        }
+    }
+}
+/**
+ * stable - checks if grid is stable
+ * @grid: grid to analize
+ * Return: 1 true o false
+ *
+ *
+ */
+int stable(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
+}
+/**
+ * sandpiles_sum - add two grids
+ * @grid1: 1st grid to add
+ * @grid2: 2nd grid to add
+ * Return: no return
+ *
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j, unstable = 0;
+    int tmp[3][3];
 
-    /* Add grid2 to grid1 */
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            grid1[i][j] += grid2[i][j];
-
-    /* Check if grid1 is unstable */
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            if (grid1[i][j] > 3)
-                unstable = 1;
-
-    /* If grid1 is unstable, topple it until it becomes stable */
-    while (unstable)
+    add(grid1, grid2);
+    while (!stable(grid1))
     {
         printf("=\n");
-        print_grid(grid1);
-
-        int topple[3][3] = {0};
-
-        for (i = 0; i < 3; i++)
-            for (j = 0; j < 3; j++)
-                if (grid1[i][j] > 3)
-                {
-                    topple[i][j] = 1;
-                }
-
-        for (i = 0; i < 3; i++)
-            for (j = 0; j < 3; j++)
-                if (topple[i][j])
-                {
-                    grid1[i][j] -= 4;
-                    if (i > 0)
-                        grid1[i - 1][j]++;
-                    if (j > 0)
-                        grid1[i][j - 1]++;
-                    if (i < 2)
-                        grid1[i + 1][j]++;
-                    if (j < 2)
-                        grid1[i][j + 1]++;
-                }
-
-        unstable = 0;
-        for (i = 0; i < 3; i++)
-            for (j = 0; j < 3; j++)
-                if (grid1[i][j] > 3)
-                    unstable = 1;
+        print_grid2(grid1);
+        tupple(grid1, tmp);
+        add(grid1, tmp);
     }
 }
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,6 +1,8 @@
-#ifndef SANDPILES_H
-#define SANDPILES_H
+#ifndef SAND_H
+#define SAND_H
+#include <stdlib.h>
+#include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
-#endif /* SANDPILES_H */
+#endif
