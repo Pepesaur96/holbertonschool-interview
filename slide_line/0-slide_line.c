faf54241344645479a41be4166efc2a272d537c8
HectorV <jjvazquez96@gmail.com>
2024-07-23 12:00:02 -0400

    Task 0 / fixing compiling errors

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,52 +1,108 @@
 #include "slide_line.h"
-/**/
-void slide_and_combine(int * line, size_t size, int direction){
-    if (direction == SLIDE_LEFT) {
-        // Slide and combine towards the left
-        for (size_t i = 0; i < size; i++) {
-            for (size_t j = i + 1; j < size; j++) {
-                if (line[i] == 0 && line[j] != 0) {
-                    line[i] = line[j];
-                    line[j] = 0;
-                } else if (line[i] == line[j] && line[i] != 0) {
-                    line[i] *= 2;
-                    line[j] = 0;
-                    break;
-                } else if (line[j] != 0) {
-                    break;
-                }
-            }
-        }
-    } else if (direction == SLIDE_RIGHT) {
-        // Slide and combine towards the right
-        for (size_t i = size; i > 0; i--) {
-            for (size_t j = i - 1; j > 0; j--) {
-                if (line[i - 1] == 0 && line[j - 1] != 0) {
-                    line[i - 1] = line[j - 1];
-                    line[j - 1] = 0;
-                } else if (line[i - 1] == line[j - 1] && line[i - 1] != 0) {
-                    line[i - 1] *= 2;
-                    line[j - 1] = 0;
-                    break;
-                } else if (line[j - 1] != 0) {
-                    break;
-                }
-            }
-        }
-    }
-}
-/*
-* slide_line - function that slides and merges an array of integers
-* @line: points to an array of integers containing size elements
-* @size: size of the array
-* @direction: direction to slide
-* Return: 1 upon success, or 0 upon failure
- */
-int slide_line(int *line, size_t size, int direction){
-        if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT) {
-        return 0;
-    }
-    slide_and_combine(line, size, direction);
-    return 1;
 
+/**
+ * slide_line - slides an array to left or right to sum numbers
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * @direction: left or right
+ * Return: 1 if is possible or 0 if it's not possible
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+	if (direction == 2)
+		return (to_left(line, size));
+	if (direction == 1)
+		return (to_right(line, size));
+	else
+		return (0);
+}
+
+/**
+ * to_left - slide to the left to sum
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * Return: 1 on success
+ */
+
+int to_left(int *line, size_t size)
+{
+	int cur = 0, nxt = 0;
+	size_t i, index = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			nxt = line[i];
+		if (cur != 0 && nxt != 0)
+		{
+			if (cur == nxt)
+			{
+				line[index++] = cur + nxt;
+				cur = 0;
+				nxt = 0;
+			}
+			else
+			{
+				line[index++] = cur;
+				cur = nxt;
+				nxt = 0;
+				if (i == size - 1)
+					line[index++] = cur;
+			}
+		}
+		if (cur != nxt && i == size - 1)
+			line[index++] = cur;
+	}
+	for (i = index; i < size; i++)
+		line[i] = 0;
+	return (1);
+}
+
+/**
+ * to_right - slides an array to sum numbers
+ *
+ * @line: line of numbers to be checked
+ * @size: size of array
+ * Return: 1 on success
+ */
+
+int to_right(int *line, size_t size)
+{
+	int cur = 0, previous = 0;
+	size_t i, index = size - 1;
+
+	for (i = size - 1; i < size; i--)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			previous = line[i];
+		if (cur != 0 && previous != 0)
+		{
+			if (cur == previous)
+			{
+				line[index--] = cur + previous;
+				cur = 0;
+				previous = 0;
+			}
+			else
+			{
+				line[index--] = cur;
+				cur = previous;
+				previous = 0;
+				if (i == 0)
+					line[index--] = cur;
+			}
+		}
+		else if (cur != previous && i == 0)
+			line[index--] = cur;
+	}
+	for (i = 0; i < index + 1; i++)
+		line[i] = 0;
+	return (1);
 }
